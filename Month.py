f084167b7cc98c853f18d14d8e7f4391ca68a97e
Thomas Zelaya <130874103+tzel28@users.noreply.github.com>
2023-11-26 16:15:53 -0500

    Add files via upload

---
+++ Month.py
@@ -0,0 +1,8 @@
+# In this python file we will make a simple calander 
+# import the libary 
+import calendar
+
+year = 2023
+month = 12 #you input the month you would like to see 
+
+print(calendar.month(year, month))
---
+++ password.py
@@ -0,0 +1,18 @@
+#In this python file we're going to create a random password generator
+
+import random
+
+password= ''
+
+print("Your password is: " +password)
+
+chars= 'abcdefghijklmnopqrustuvwxyz1234567890*&%$#@!()?-' #create a list of possibilities 
+
+
+
+for i in range(10): #range for how long this password is going to be 
+    password +=random.choice(chars) 
+    
+print(password)
+
+
