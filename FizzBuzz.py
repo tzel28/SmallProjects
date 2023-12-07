e14622edd5c38f8f4bb4d7bbfc4d3027ab0b34c4
Thomas Zelaya <130874103+tzel28@users.noreply.github.com>
2023-12-07 16:49:39 -0500

    Add files via upload

---
+++ FizzBuzz.py
@@ -0,0 +1,12 @@
+num = 100
+
+for i in range (1, num + 1):
+
+  if i % 3 == 0 and i % 5 == 0:
+    print('FizzBuzz')
+  elif i % 5 == 0:
+    print('Buzz')
+  elif i % 3 == 0:
+    print("Fizz")
+  else:
+      print(i)
